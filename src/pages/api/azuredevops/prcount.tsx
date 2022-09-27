00fc5dbef7b67ead05fc79d16bf6a8a880f32e6c
MaheshtheDev <mahesh.svmr@gmail.com>
2022-09-27 10:25:24 +0530

    build fix

--- src/pages/api/azuredevops/prcount.tsx
+++ src/pages/api/azuredevops/prcount.tsx
@@ -5,7 +5,7 @@ import * as Sentry from "@sentry/browser";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   Sentry.captureMessage("Pr count handler started");
   if (req.method == "GET") {
-    let count = 2;
+    const count = 2;
     return res.status(200).json({ count });
   } else {
     throw new Error("Only GET method is allowed");
